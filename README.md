9fcbb9bd600a337eb5456d4e858f14c12775377c
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2023-03-27 21:44:58 -0700

    🎉Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Poké Quiz 
+
+## About
+_A quick Vanilla JavaScript quiz site_ 🍦 
+
+A quiz to help you figure out who should be your next Pokémon? 👀
+
+
+## Tech Used
+- JavaScript
+- [Pokemon API](https://pokeapi.co/)
---
+++ data.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        question: 'What\'s your favorite element?',
+        options: [
+            'Normal',
+            'Fire',
+            'Water',
+            'Grass',
+            'Flying',
+            'Fighting',
+            'Poison',
+            'Electric',
+            'Ground',
+            'Rock',
+            'Psychic',
+            'Ice',
+            'Bug',
+            'Ghost',
+            'Steel',
+            'Dragon',
+            'Dark',
+            'Fairy'
+        ]
+    }
+];
+
+export default quiz;
